c41325c7bc59fbcebc0b940ab7318367fdd065f3
Hariom Sharma <punits152@gmail.com>
2021-09-09 21:25:58 +0530

    Trivial Changes

--- app_package/routes.py
+++ app_package/routes.py
@@ -5,26 +5,25 @@ from app_package.forms import PredictionForm
 import pickle
 import numpy as np
 
-with open("../Model_and_analysis/best_estimator_poly",'r') as f:
+with open("Model_and_analysis/regr_model",'rb') as f:
     estimator = pickle.load(f)
 
-
-# All the routes of our application are here
-@app.route("/")
-@app.route("/home")
+# All the routes of our application are here 
+@app.route("/", methods=["GET","POST"])
+@app.route("/home", methods=["GET","POST"])
 def home_page():
     form = PredictionForm()
     if form.validate_on_submit() :
-        X = np.array([form.fat_regular.data,form.price.data,form.visibility.data,form.location_type_tier2.data,form.supermarket_type1.data,form.supermarket_type2.data,form.supermarket_type3.data])
-        y_hat = estimator.predict(X)
-        return redirect(url_for("result_page"),y_hat)
-
+        X = np.array([[form.fat_regular.data,form.price.data,form.visibility.data,form.location_type_tier2.data,form.supermarket_type1.data,form.supermarket_type2.data,form.supermarket_type3.data]])
+        y_hat = estimator.predict(X)[0]
+        return render_template("result.html", y_hat=y_hat)
+    
+    elif form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f"There wan an error with creating user: {err_msg}")
+    
     return render_template("home_page.html",form=form)
-
+    
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
-
-@app.route("/results")
-def result_page():
-    return render_template("result.html")
